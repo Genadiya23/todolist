01e478a034e482edd68aa9ae9e8e864ce52ec5fe
Genadiya23 <sivskovayana@gmail.com>
2024-12-12 16:09:20 -0500

    Backend for MongoDB is done

--- todolist/backend/Server.js
+++ todolist/backend/Server.js
@@ -4,7 +4,16 @@ const mongoose = require ('mongoose')
 require ('dotenv').config()
 
 const app = express()
-const PORT = process.env.port || 5113
+const PORT = process.env.PORT || 5119
+
+require('dotenv').config();
+
+console.log('MONGODB_URL:', process.env.MONGODB_URL); // Log the URL
+
+mongoose.connect(process.env.MONGODB_URL)
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
 
 mongoose
 .connect(process.env.MONGODB_URL)
